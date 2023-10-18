9ae14487b6fd522699d26339c51d767c4f56b3dc
magparth <148080706+magparth@users.noreply.github.com>
2023-10-18 19:54:01 +0530

    Password Generator using python
    I made this random password generator using python. The libraries i used were random and tkinter. I learnt how to use these libraries and also how to manipulate them as per requirements in the code.

---
+++ passwordgenerator.py
@@ -0,0 +1,23 @@
+import tkinter as tk 
+import random
+import string
+def genpass(length):
+    chartacter= string.ascii_letters + string.digits + string.punctuation
+    password=''.join(random.choice(chartacter)for _ in range(length))
+    return password
+def genpassbuton():
+    length=int(length_entry.get())
+    password=genpass(length)
+    passdisplay.config(text="generated password is:"+password)
+app=tk.Tk()
+app.title("password generator")
+length_label=tk.Label(app,text= "enter the length:")
+length_label.pack()
+length_entry=tk.Entry(app)
+length_entry.pack()
+generatebutton=tk.Button(app,text="generate password:",coomand=genpassbuton)
+generatebutton.pack()
+passdisplay=tk.Label(app,text='')
+passdisplay.pack()
+app.mainloop()
+    
